23ad7a14b57d34072c4afb454debfc415bad6600
ania.liulka <anialiulka@gmail.com>
2023-05-20 20:22:41 -0400

    Completed task 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const buttonStartGenerating = document.querySelector('[data-start]');
+const buttonStopGenerating = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+
+buttonStopGenerating.disabled = true;
+
+buttonStartGenerating.addEventListener('click', onClickStartGenerating);
+buttonStopGenerating.addEventListener('click', onClickStopGenerating);
+
+function onClickStartGenerating() {
+  buttonStartGenerating.disabled = true;
+  buttonStopGenerating.disabled = false;
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onClickStopGenerating() {
+  clearInterval(timerId);
+  buttonStartGenerating.disabled = false;
+  buttonStopGenerating.disabled = true;
+}
